e21186ddbb43be0ffb1eb0983070b3d5a81d2402
guille garcia <ggarciacardiel@gmail.com>
2023-03-21 12:34:05 +0100

    it2 home getShows > Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
-export default function Card() {
+export default function Card({ show }) {
+  console.log(show.title)
   return (
     // Use to display each one of the movies
-    <div>Card</div>
+    <div>
+      <img style={{width: '200px'}} src={show.image} alt={show.title} />
+      <h2>
+        <Link to={`/shows/${show._id}`}>{show.title}</Link>
+      </h2>
+    </div>
   )
 }
