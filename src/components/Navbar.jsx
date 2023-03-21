84c363f63e96743366bbd917a1a95a718e2b43e6
guille garcia <ggarciacardiel@gmail.com>
2023-03-21 11:42:39 +0100

    it1 nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div>
-      {/* ITERATION 1: Should use react-router-dom NavLinks to browse through the different pages */}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/new">New show</NavLink>
+      <NavLink onClick={() => navigate(-1)}>Back</NavLink>
     </div>
   )
 }
